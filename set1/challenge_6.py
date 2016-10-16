9095e0e685c5df78038d845894ac37081ac77de7
Sandra Escandor-O'Keefe <sokeefe@fastly.com>
2016-10-15 22:48:29 -0230

    Rough outline for solution

--- set1/challenge_6.py
+++ set1/challenge_6.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python27
 
 import collections
+from challenge_3 import Decrypt_Key_Solver
 from bitstring import BitArray
 
 class Hamming_Distance_Calculator():
@@ -43,6 +44,70 @@ class Hamming_Distance_Calculator():
             resulting_diff_stream.append(BitArray(bool=result))
 
         return resulting_diff_stream.count(1)
+
+class Keysize_Estimator():
+
+    def __init__(self, cipher_data):
+        self.cipher_data = cipher_data
+        self.size_range = range(2, 40)
+        self.key_to_edit_dist = dict()
+
+    def _get_smallest_keys_to_edit_dist():
+        ordered_by_edit_dist = collections.OrderedDict(sorted(self.key_to_normalized_dist.values(), reverse=True)
+        return ordered_by_edit_dist[0:3] 
+            
+    def get_edit_distances():
+        for keysize in self.size_range:
+            num_bits = keysize * 8
+            first = self.cipher_data[0:num_bits]
+            second = self.cipher_data[num_bits:2*num_bits]
+            hamming_dist = Hamming_Distance_Calculator(first, secod)
+            normalized = hamming_dist / keysize
+            self.key_to_edit_dist[keysize] = normalized
+
+         return self._get_smallest_keys_to_edit_dist()
+
+class Repeating_XOR_Breaker():
+    
+    def __init__(self, cipher_data):
+        self.cipher_data = cipher_data
+        self.keysize_estimator = Keysize_Estimator(self.cipher_data)
+        self.key_to_transposed_streams = dict()
+
+    def _transpose(self):
+        candidate_keysizes = self.keysize_estimator.get_edit_distances()
+
+        """
+        Each row in transposed_streams corresponds to the nth byte
+        of a block. For example, transposed_streams[0] corresponds to
+        the 1st byte of every keysize block; transposed_streams[1] 
+        corresponds to the 2nd byte of every keysize block; and so on. 
+        """
+        
+        for candidate_keysize in candidate_keysizes:
+            num_blocks = self.cipher_data.len/candidate_keysize
+            transposed_streams = list(num_blocks)
+
+            for bucket in range(0, candidate_keysize):
+                skip_by_bits = candidate_keysize * 8
+                bucket_in_bits = bucket * 8
+                for i in xrange(bucket_in_bits, self.cipher_data.len, skip_by_bits): 
+                    transposed_streams[bucket].append(self.cipher_data[i])
+
+            self.key_to_transposed_streams[candidate_keysize] = transposed_streams
+
+     def solve(self):
+         candidate_keys = list()
+         self._transpose()
+         for candidate_keysize, transposed_stream in self.key_to_transposed_streams.iteritems():
+             key = list()
+             for bucket in transposed_stream:
+                 solver = Decrypt_Key_Solver(bucket)
+                 key.append(solver.get_key_used())
+
+             candidate_keys.append(key) 
+
+         return candidate_keys
 
 def main():
     
