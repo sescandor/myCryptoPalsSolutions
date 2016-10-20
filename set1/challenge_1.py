b69411a176422fc0ece40334691137045d196947
Sandra Escandor-O'Keefe <sokeefe@fastly.com>
2016-10-20 16:10:16 -0230

    Make class for hex to base64 conversion

--- set1/challenge_1.py
+++ set1/challenge_1.py
@@ -2,17 +2,25 @@ import binascii
 import base64
 import sys
 
+class Hex_To_Base64():
+
+    def __init__(self, hex_to_convert):
+        self.to_convert = hex_to_convert
+
+    def convert(self):
+        to_bin = binascii.a2b_hex(self.to_convert)
+
+        to_b64 = (binascii.b2a_base64(to_bin)).rstrip('\n')
+
+        return to_b64
+
 if __name__ == '__main__':
-  stringToConv = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    string_to_convert = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 
-  stringShouldBe = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    string_should_be = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
-  hex = stringToConv.decode("hex") 
-  
-  toConvert_toBin = binascii.a2b_hex(stringToConv)
+    toConvert_b64 = Hex_To_Base64(string_to_convert).convert() 
 
-  toConvert_b64 = (binascii.b2a_base64(toConvert_toBin)).rstrip('\n')
-
-  if toConvert_b64 == stringShouldBe:
-    print "Successfully converted to base64!"
+    if toConvert_b64 == string_should_be:
+        print "Successfully converted to base64!"
 
