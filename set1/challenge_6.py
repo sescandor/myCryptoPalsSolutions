e0aab88abd40bd065da6d54b6e0812a3c794f48a
Sandra Escandor-O'Keefe <sokeefe@fastly.com>
2016-10-25 22:39:56 -0230

    challenge 6 so far - TODO: need to decrypt with the candidate keys in order to see what is up

--- set1/challenge_6.py
+++ set1/challenge_6.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python27
 
 import collections
-from challenge_3 import Decrypt_Key_Solver
+from challenge_1 import Base64_To_Hex
+from challenge_3 import Decrypter, Decrypt_Key_Solver
+from challenge_4 import Data_reader
 from bitstring import BitArray
+from operator import itemgetter
+
 
 class Hamming_Distance_Calculator():
 
-    def __init__(self, str1, str2):
-        self.bit_str1 = BitArray(hex=str1.encode('hex')) 
-        self.bit_str2 = BitArray(hex=str2.encode('hex')) 
+    def __init__(self, bit_str1, bit_str2):
+        self.bit_str1 = bit_str1
+        self.bit_str2 = bit_str2
 
     def _pre_process(self):
-        
         if self.bit_str1.len != self.bit_str2.len:
             len_map = {self.bit_str1.len: self.bit_str1, self.bit_str2.len: self.bit_str2}
             len_map = collections.OrderedDict(sorted(len_map.items(), reverse=True))
@@ -52,17 +55,21 @@ class Keysize_Estimator():
         self.size_range = range(2, 40)
         self.key_to_edit_dist = dict()
 
-    def _get_smallest_keys_to_edit_dist():
-        ordered_by_edit_dist = collections.OrderedDict(sorted(self.key_to_normalized_dist.values(), reverse=True))
+    def _get_smallest_keys_to_edit_dist(self):
+        key_list = sorted(self.key_to_edit_dist.items(), key=itemgetter(1))
+        keys_with_smallest_edit_dist = list() 
+       
+        for k,v in key_list:
+            keys_with_smallest_edit_dist.append(k) 
 
-        return ordered_by_edit_dist[0:3] 
+        return keys_with_smallest_edit_dist[0:4] 
             
-    def get_edit_distances():
+    def get_edit_distances(self):
         for keysize in self.size_range:
             num_bits = keysize * 8
             first = self.cipher_data[0:num_bits]
             second = self.cipher_data[num_bits:2*num_bits]
-            hamming_dist = Hamming_Distance_Calculator(first, secod)
+            hamming_dist = Hamming_Distance_Calculator(first, second).get_distance()
             normalized = hamming_dist / keysize
             self.key_to_edit_dist[keysize] = normalized
 
@@ -96,13 +103,13 @@ class Repeating_XOR_Breaker():
         """
         
         for candidate_keysize in self.candidate_keysizes:
-            transposed_streams = {bucket: [] for bucket in
+            transposed_streams = {bucket: BitArray() for bucket in
                                   range(candidate_keysize)}
 
             skip_by_bits = (candidate_keysize) * 8
-            print "skip_by_bits:", skip_by_bits
+            
             for i in xrange(0, self.cipher_data.len, skip_by_bits):
-                print "processing", self.cipher_data[i:]
+                
                 n = 0
                 for stream in transposed_streams:
                     start = i + n
@@ -118,14 +125,51 @@ class Repeating_XOR_Breaker():
         self._get_candidate_keysizes()
         self._transpose()
         for candidate_keysize, transposed_stream in self.key_to_transposed_streams.iteritems():
+            print "candidate_keysize:", candidate_keysize
             key = list()
             for bucket in transposed_stream:
-                solver = Decrypt_Key_Solver(bucket)
-                key.append(solver.get_key_used())
+                stream_to_solve = '0x' + transposed_stream[bucket].hex
+                solver = Decrypt_Key_Solver(stream_to_solve)
+                solver.solve()
+                key_used = solver.get_key_used()
+                key.append(key_used)
 
             candidate_keys.append(key)
 
         return candidate_keys
+
+
+class ExtendedKeyDecrypter(Decrypter):
+
+    def __init__(self):
+        Decrypter.__init__(self)
+        self.key_len = 0
+
+    def set_decrypt_key(self, key):
+        self.XOR_to_use = BitArray(hex=key)
+        self.key_len = self.XOR_to_use.len
+
+    def decrypt(self):
+        result = BitArray(hex='0x00')
+        skip_by = self.key_len
+        for i in xrange(0, self.cipher_stream.len, skip_by):
+            xor_res = self.cipher_stream[i:skip_by] ^ self.XOR_to_use
+            result.append(xor_res)
+            skip_by += self.key_len
+
+        return result
+
+
+def test_transpose():
+    test_breaker = Repeating_XOR_Breaker(BitArray(hex="0x12341234"))
+    test_list = list()
+    test_list.append(2)
+    test_breaker._set_candidate_keysizes(test_list)
+    test_breaker._transpose()
+
+    streams = test_breaker._get_keysize_to_transposed_streams()
+    print streams[2]
+
 
 def main():
     """
@@ -134,14 +178,29 @@ def main():
     3. Convert hex string representation to BitArray.
     4. Feed into Repeating_XOR_Breaker
     """
-    test_breaker = Repeating_XOR_Breaker(BitArray(hex="0x12341234"))
-    test_list = list()
-    test_list.append(2)
-    test_breaker._set_candidate_keysizes(test_list)
-    test_breaker._transpose()
-    
-    streams = test_breaker._get_keysize_to_transposed_streams()
-    print streams[2]  
+    cipher_source = "https://cryptopals.com/static/challenge-data/6.txt"
+    cipher_reader = Data_reader(cipher_source)
+
+    data = cipher_reader.get_data()
+    data = ''.join(data)
+    converted = Base64_To_Hex(data).convert()
+    data_to_break = BitArray(hex=converted)
+    breaker = Repeating_XOR_Breaker(data_to_break)
+
+    candidate_keys = breaker.solve()
+
+    print "Candidate Keys:", candidate_keys
+
+    for key_set in candidate_keys:
+        whole_key = BitArray(hex='0x00')
+        for key in key_set:
+            whole_key.append(BitArray(int=key, length=8)) 
+
+        print "whole_key:", whole_key
+        decrypter = ExtendedKeyDecrypter()
+        decrypter.set_decrypt_key(whole_key.hex)
+        print decrypter.decrypt()
+
 
 if __name__ == '__main__':
     main()
